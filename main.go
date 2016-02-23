ea8154a3664d44c5a2956ad7b7dca0c2a90d6dd3
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2016-02-23 15:00:30 +0200

    cleanup

--- main.go
+++ main.go
@@ -9,33 +9,13 @@ import (
 	"strings"
 )
 
-func lint(file string) {
-	stdout, err := exec.Command("php", "-l", file).Output()
-	if err != nil {
-		message := string(stdout)
-		message = strings.TrimSpace(message)
-		fmt.Println(message)
-	}
-}
-
 func main() {
 	err := exec.Command("which", "php").Run()
 	if err != nil {
 		log.Fatal("command not found: php")
 	}
 
-	files := make([]string, 0)
-
-	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		path = strings.TrimSpace(path)
-		if path == "" || strings.HasPrefix(path, "vendor") || strings.HasPrefix(path, "node_modules") {
-			return err
-		}
-		if strings.HasSuffix(path, ".php") {
-			files = append(files, path)
-		}
-		return err
-	})
+	files := getFiles()
 
 	jobs := make(chan string, len(files))
 	results := make(chan bool, len(files))
@@ -55,9 +35,39 @@ func main() {
 	}
 }
 
+func getFiles() []string {
+	files := make([]string, 0)
+
+	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		path = strings.TrimSpace(path)
+		if isLintable(path) {
+			files = append(files, path)
+		}
+		return err
+	})
+
+	return files
+}
+
+func isLintable(path string) bool {
+	if path == "" || strings.HasPrefix(path, "vendor") || strings.HasPrefix(path, "node_modules") {
+		return false
+	}
+	return strings.HasSuffix(path, ".php")
+}
+
 func worker(id int, jobs <-chan string, results chan<- bool) {
 	for j := range jobs {
 		lint(j)
 		results <- true
+	}
+}
+
+func lint(file string) {
+	stdout, err := exec.Command("php", "-l", file).Output()
+	if err != nil {
+		message := string(stdout)
+		message = strings.TrimSpace(message)
+		fmt.Println(message)
 	}
 }
